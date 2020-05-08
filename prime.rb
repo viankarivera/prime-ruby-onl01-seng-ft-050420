d7675dd74c7f30ce56988638f8849d2dfce2fac1
Vianka Rivera <viankalrivera@gmail.com>
2020-05-08 22:05:13 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(number)
+  if number <= 1 || number == 0 || number == 1
+    return false
+  elsif
+    (2..number - 1).each do |i|
+      if number % i == 0
+      return false
+    end
+  end
+end
+true
+end
